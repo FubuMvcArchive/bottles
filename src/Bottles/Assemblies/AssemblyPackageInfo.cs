ecfd83eab24d67e57adcf3d0fcf946f1d79863fe
Jeremy Miller <jeremydmiller@yahoo.com>
2011-05-16 08:35:03 -0500

    made AssemblyPackageInfo.CreateFor(file), and more robust

--- src/Bottles/Assemblies/AssemblyPackageInfo.cs
+++ src/Bottles/Assemblies/AssemblyPackageInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using Bottles.Exploding;
@@ -6,6 +7,7 @@ using FubuCore;
 
 namespace Bottles.Assemblies
 {
+
     public class AssemblyPackageInfo : IPackageInfo
     {
         private readonly Assembly _assembly;
@@ -24,6 +26,33 @@ namespace Bottles.Assemblies
             exploder.ExplodeAssembly(PackageRegistry.GetApplicationDirectory(), assembly, package.Files);
 
             return package;
+        }
+
+        // TODO -- remove duplication with AssemblyLoader
+        public static AssemblyPackageInfo CreateFor(string fileName)
+        {
+            var assembly = loadPackageAssemblyFromAppBinPath(fileName);
+            return CreateFor(assembly);
+        }
+
+        private static Assembly loadPackageAssemblyFromAppBinPath(string file)
+        {
+            var assemblyName = Path.GetFileNameWithoutExtension(file);
+            var appBinPath = AppDomain.CurrentDomain.SetupInformation.PrivateBinPath ?? AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            if (!Path.GetDirectoryName(file).EqualsIgnoreCase(appBinPath))
+            {
+                var destFileName = FileSystem.Combine(appBinPath, Path.GetFileName(file));
+                if (shouldUpdateFile(file, destFileName))
+                {
+                    File.Copy(file, destFileName, true);
+                }
+            }
+            return Assembly.Load(assemblyName);
+        }
+
+        private static bool shouldUpdateFile(string source, string destination)
+        {
+            return !File.Exists(destination) || File.GetLastWriteTimeUtc(source) > File.GetLastWriteTimeUtc(destination);
         }
 
         public PackageFiles Files
