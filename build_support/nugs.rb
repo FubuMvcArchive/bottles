d76b53009bd20ea6948eee6688b9a122ea70b21b
Dru Sellers <dru@drusellers.com>
2011-05-16 11:38:05 -0500

    adding nuget support

---
+++ build_support/nugs.rb
@@ -0,0 +1,34 @@
+namespace :nug do
+	@nuget = "lib/nuget.exe"
+	
+	@packname = 'fubumvc'
+	@files = ['FubuCore','FubuLocalization'].map {|name| name + ".dll"}
+	@nugroot = File.expand_path("/nugs")
+	
+	desc "Build the nuget package"
+	task :build do
+		sh "#{@nuget} pack packaging/nuget/bottles.nuspec -o #{ARTIFACTS} -Version #{BUILD_NUMBER}"
+	end
+
+	
+	desc "pulls new NuGet updates from your local machine"
+	task :pull, [:location] => [:build] do |t, args|
+		args.with_defaults(:location => 'local')
+		location = args[:location]
+		
+		@files.each do |f|
+			#copy to 
+		end
+	end
+		
+	desc "pushes new NuGet udates to your local machine"
+	task :push, [:location] => [:build] do |t, args|
+		args.with_defaults(:location => 'local')
+		location = args[:location]
+			
+		Dir["#{ARTIFACTS}/*.nupkg"].each do |fn|		
+			FileUtils.cp fn, @nugroot
+		end
+	end
+	
+end
