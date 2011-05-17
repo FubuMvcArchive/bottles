50a65b433f8b3e4eda0f35b1273dd9df384c8076
Brandon Behrens <brandonbehrens@gmail.com>
2011-05-17 16:02:49 -0500

    Added an IISWebsiteDeployer for deploying generic websites.

---
+++ src/Bottles.Deployers.Iis/IISWebsiteDeployer.cs
@@ -0,0 +1,22 @@
+﻿using Bottles.Deployment;
+using Bottles.Deployment.Directives;
+using Bottles.Deployment.Runtime;
+using Bottles.Diagnostics;
+
+namespace Bottles.Deployers.Iis
+{
+    public class IISWebsiteDeployer : IDeployer<Website>
+    {
+        private readonly IisWebsiteCreator _websiteCreator;
+
+        public IISWebsiteDeployer(IisWebsiteCreator websiteCreator)
+        {
+            _websiteCreator = websiteCreator;
+        }
+
+        public void Execute(Website directive, HostManifest host, IPackageLog log)
+        {
+            _websiteCreator.Create(directive);
+        }
+    }
+}
