3b7b567e78ee56648f34f2e9fdbceab6b6fb336e
Jeremy Miller <jeremydmiller@yahoo.com>
2011-05-23 15:00:46 -0500

    installers directive, just delegates to InstallCommand

---
+++ src/Bottles.Deployment/Deployers/Installers/InstallersDirective.cs
@@ -0,0 +1,55 @@
+using System;
+using Bottles.Deployment.Commands;
+using Bottles.Deployment.Runtime;
+using Bottles.Diagnostics;
+using FubuCore;
+
+namespace Bottles.Deployment.Deployers.Installers
+{
+    public class InstallersDirective : IDirective
+    {
+        private readonly InstallInput _input = new InstallInput();
+
+        public string AppDirectory
+        {
+            get { return _input.AppFolder; }
+            set { _input.AppFolder = value; }
+        }
+
+        public InstallMode Mode
+        {
+            get
+            {
+                return _input.ModeFlag;
+            }
+            set { _input.ModeFlag = value; }
+        }
+
+        public string LogFile
+        {
+            get
+            {
+                return _input.LogFileFlag;
+            }
+            set
+            {
+                _input.LogFileFlag = value;
+            }
+        }
+
+        public InstallInput Input
+        {
+            get { return _input; }
+        }
+    }
+
+    public class InstallersDeployer : IDeployer<InstallersDirective>
+    {
+        public void Execute(InstallersDirective directive, HostManifest host, IPackageLog log)
+        {
+            log.Trace(directive.Input.Title());
+            log.Trace("Writing the installer log file to " + directive.Input.LogFileFlag.ToFullPath());
+            new InstallCommand().Execute(directive.Input);
+        }
+    }
+}
