42fb927f1b7c0157fc153a55fc381cd82c37c9d5
Brandon Behrens <brandonbehrens@gmail.com>
2011-06-03 11:13:54 -0500

    Moved some stuff from fubu into the iis website deployer.

--- src/Bottles.Deployers.Iis/IISWebsiteDeployer.cs
+++ src/Bottles.Deployers.Iis/IISWebsiteDeployer.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using Bottles.Deployment;
 using Bottles.Deployment.Runtime;
+using Bottles.Deployment.Runtime.Content;
 using Bottles.Diagnostics;
 using FubuCore;
 
@@ -8,16 +10,84 @@ namespace Bottles.Deployers.Iis
     public class IisWebsiteDeployer : IDeployer<Website>
     {
         private readonly IisWebsiteCreator _websiteCreator;
+        private readonly IBottleMover _bottleMover;
 
-        public IisWebsiteDeployer(IisWebsiteCreator websiteCreator)
+        public IisWebsiteDeployer(IisWebsiteCreator websiteCreator, IBottleMover bottleMover)
         {
             _websiteCreator = websiteCreator;
+            _bottleMover = bottleMover;
         }
 
         public void Execute(Website directive, HostManifest host, IPackageLog log)
         {
             directive.VDirPhysicalPath = directive.VDirPhysicalPath.ToFullPath();
             _websiteCreator.Create(directive);
+
+            var destination = new WebsiteBottleDestination(directive.VDirPhysicalPath);
+            _bottleMover.Move(log, destination, host.BottleReferences);
         }
     }
+
+    public class WebsiteBottleDestination : IBottleDestination
+    {
+        private readonly string _physicalPath;
+
+        public WebsiteBottleDestination(string physicalPath)
+        {
+            _physicalPath = physicalPath;            
+        }
+
+        public virtual IEnumerable<BottleExplosionRequest> DetermineExplosionRequests(PackageManifest manifest)
+        {
+            switch (manifest.Role)
+            {
+                case BottleRoles.Binaries:
+                    yield return new BottleExplosionRequest
+                    {
+                        BottleDirectory = BottleFiles.BinaryFolder,
+                        BottleName = manifest.Name,
+                        DestinationDirectory = FileSystem.Combine(_physicalPath, BottleFiles.BinaryFolder)
+                    };
+                    break;
+
+                case BottleRoles.Config:
+                    yield return new BottleExplosionRequest()
+                    {
+                        BottleDirectory = BottleFiles.ConfigFolder,
+                        BottleName = manifest.Name,
+                        DestinationDirectory = FileSystem.Combine(_physicalPath, BottleFiles.ConfigFolder)
+                    };
+                    break;
+
+                case BottleRoles.Module:                    
+                    yield return new BottleExplosionRequest
+                    {
+                        BottleDirectory = BottleFiles.BinaryFolder,
+                        BottleName = manifest.Name,
+                        DestinationDirectory = _physicalPath.AppendPath(BottleFiles.BinaryFolder)
+                    };
+                    break;
+
+                case BottleRoles.Application:
+                    yield return new BottleExplosionRequest
+                    {
+                        BottleName = manifest.Name,
+                        BottleDirectory = BottleFiles.BinaryFolder,
+                        DestinationDirectory = FileSystem.Combine(_physicalPath, BottleFiles.BinaryFolder)
+                    };
+
+                    yield return new BottleExplosionRequest
+                    {
+                        BottleName = manifest.Name,
+                        BottleDirectory = BottleFiles.WebContentFolder,
+                        DestinationDirectory = _physicalPath
+                    };
+
+                    break;
+
+                default:
+                    yield break;
+            }
+        }
+    }    
 }
---
+++ src/Bottles.Tests/Deployment/Deployers/WebsiteBottleDestinationTester.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Bottles.Deployers.Iis;
+using Bottles.Deployment.Runtime.Content;
+using Bottles.Diagnostics;
+using FubuCore;
+using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace Bottles.Tests.Deployment.Deployers
+{
+    [TestFixture]
+    public class WebsiteBottleDestinationTester
+    {
+        private WebsiteBottleDestination theDestination;
+        private string theRootFolder;
+
+        [SetUp]
+        public void SetUp()
+        {
+            theRootFolder = "deployedPath";
+            theDestination = new WebsiteBottleDestination(theRootFolder);
+        }
+
+        [Test]
+        public void create_request_for_binaries()
+        {
+            var request = theDestination.DetermineExplosionRequests(new PackageManifest()
+            {
+                Role = BottleRoles.Binaries,
+                Name = "the bottle name"
+            }).Single();
+
+            request.BottleDirectory.ShouldEqual(BottleFiles.BinaryFolder);
+            request.DestinationDirectory.ShouldEqual(FileSystem.Combine(theRootFolder, BottleFiles.BinaryFolder));
+            request.BottleName.ShouldEqual("the bottle name");
+        }
+
+        [Test]
+        public void create_request_for_config()
+        {
+            var request = theDestination.DetermineExplosionRequests(new PackageManifest()
+            {
+                Role = BottleRoles.Config,
+                Name = "the bottle name"
+            }).Single();
+
+            request.BottleDirectory.ShouldEqual(BottleFiles.ConfigFolder);
+            request.DestinationDirectory.ShouldEqual(FileSystem.Combine(theRootFolder, BottleFiles.ConfigFolder));
+            request.BottleName.ShouldEqual("the bottle name");
+        }
+
+        [Test]
+        public void create_request_for_module()
+        {
+            var requests = theDestination.DetermineExplosionRequests(new PackageManifest()
+            {
+                Role = BottleRoles.Module,
+                Name = "the bottle name"
+            });
+
+            var secondRequet = requests.First();
+            secondRequet.BottleDirectory.ShouldEqual(BottleFiles.BinaryFolder);
+            secondRequet.DestinationDirectory.ShouldEqual(theRootFolder.AppendPath(BottleFiles.BinaryFolder));
+        }
+
+        [Test]
+        public void create_requests_for_module()
+        {
+            var requests = theDestination.DetermineExplosionRequests(new PackageManifest()
+            {
+                Role = BottleRoles.Application,
+                Name = "the bottle name"
+            });
+
+            var expected = new List<BottleExplosionRequest>{
+                new BottleExplosionRequest(new PackageLog()){BottleName = "the bottle name", BottleDirectory = BottleFiles.BinaryFolder, DestinationDirectory = FileSystem.Combine(theRootFolder, BottleFiles.BinaryFolder)},
+                new BottleExplosionRequest(new PackageLog()){BottleName = "the bottle name", BottleDirectory = BottleFiles.WebContentFolder, DestinationDirectory = theRootFolder},
+            };
+
+            requests.ShouldHaveTheSameElementsAs(expected);
+        }
+    }
+}
