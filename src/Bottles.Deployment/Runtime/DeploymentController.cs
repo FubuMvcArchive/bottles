c19be2abb552dd7fc535b96f325a860713e22e8e
Jeremy Miller <jeremydmiller@yahoo.com>
2011-06-15 14:32:53 -0500

    Deleting the Staging directory after deployment.  Closes gh-8.

--- src/Bottles.Deployment/Runtime/DeploymentController.cs
+++ src/Bottles.Deployment/Runtime/DeploymentController.cs
@@ -4,23 +4,25 @@ using Bottles.Deployment.Parsing;
 using Bottles.Deployment.Runtime.Content;
 using Bottles.Diagnostics;
 using System.Linq;
+using FubuCore;
 
 namespace Bottles.Deployment.Runtime
 {
     public class DeploymentController : IDeploymentController
     {
         private readonly IBottleRepository _bottles;
+        private readonly IFileSystem _system;
         private readonly IDirectiveRunnerFactory _factory;
         private readonly IProfileReader _reader;
         private readonly IDiagnosticsReporter _reporter;
 
-        public DeploymentController(IProfileReader reader, IDiagnosticsReporter reporter,
-                                    IDirectiveRunnerFactory factory, IBottleRepository bottles)
+        public DeploymentController(IProfileReader reader, IDiagnosticsReporter reporter, IDirectiveRunnerFactory factory, IBottleRepository bottles, IFileSystem system)
         {
             _reader = reader;
             _reporter = reporter;
             _factory = factory;
             _bottles = bottles;
+            _system = system;
         }
 
         // TODO -- end to end tests on this monster -- including failure cases
@@ -28,21 +30,26 @@ namespace Bottles.Deployment.Runtime
         {
             // need to log inside of reader
             var plan = BuildPlan(options);
+            
+            try
+            {
+                var runners = _factory.BuildRunners(plan);
 
-            var runners = _factory.BuildRunners(plan);
+                int totalCount = runners.Sum(x => x.InitializerCount + x.DeployerCount + x.FinalizerCount);
+                LogWriter.StartSteps(totalCount, "Running all directives");
 
-            int totalCount = runners.Sum(x => x.InitializerCount + x.DeployerCount + x.FinalizerCount);
-            LogWriter.StartSteps(totalCount, "Running all directives");
+                runners.Each(x => x.InitializeDeployment());
+                runners.Each(x => x.Deploy());
+                runners.Each(x => x.FinalizeDeployment());
+
+                _reporter.WriteReport(options, plan);
+            }
+            finally
+            {
+                _system.DeleteDirectory(plan.Settings.StagingDirectory);
+            }
 
             
-
-            runners.Each(x => x.InitializeDeployment());
-            runners.Each(x => x.Deploy());
-            runners.Each(x => x.FinalizeDeployment());
-
-            _reporter.WriteReport(options, plan);
-
-
         }
 
         public DeploymentPlan BuildPlan(DeploymentOptions options)
