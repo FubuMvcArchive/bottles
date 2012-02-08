0158b4c251932d7572fbc02a70cb6c63b12ce054
Dru Sellers <dru@drusellers.com>
2012-02-08 13:43:22 -0600

    adding the ability to start/stop the app pool to the website deployers

--- src/Bottles.Deployers.Iis/WebAppOfflineFinalizer.cs
+++ src/Bottles.Deployers.Iis/WebAppOfflineFinalizer.cs
@@ -1,8 +1,8 @@
-using System;
 using Bottles.Deployment;
 using Bottles.Deployment.Runtime;
 using Bottles.Diagnostics;
 using FubuCore;
+using Microsoft.Web.Administration;
 
 namespace Bottles.Deployers.Iis
 {
@@ -18,7 +18,28 @@ namespace Bottles.Deployers.Iis
         public void Execute(Website directive, HostManifest host, IPackageLog log)
         {
             _fileSystem.DeleteFile(FileSystem.Combine(directive.VDirPhysicalPath, "app_offline.htm"));
+
+            restartPools(directive, log);
         }
+
+        private void restartPools(Website directive, IPackageLog log)
+        {
+            using(var sm = new ServerManager())
+            {
+                foreach(var pool in sm.ApplicationPools)
+                {
+                    if (!pool.Name.EqualsIgnoreCase(directive.AppPool)) continue;
+                    
+                    if(pool.State == ObjectState.Stopped)
+                    {
+                        log.Trace("Starting pool '{0}'", pool.Name);
+
+                        pool.Start();
+                    }
+                }
+            }
+        }
+
 
         public string GetDescription(Website directive)
         {
--- src/Bottles.Deployers.Iis/WebAppOfflineInitializer.cs
+++ src/Bottles.Deployers.Iis/WebAppOfflineInitializer.cs
@@ -2,6 +2,7 @@ using Bottles.Deployment;
 using Bottles.Deployment.Runtime;
 using Bottles.Diagnostics;
 using FubuCore;
+using Microsoft.Web.Administration;
 
 namespace Bottles.Deployers.Iis
 {
@@ -20,12 +21,35 @@ namespace Bottles.Deployers.Iis
             _fileSystem.CreateDirectory(directive.WebsitePhysicalPath);
             _fileSystem.CreateDirectory(directive.VDirPhysicalPath);
 
+
+            stopPools(directive, log);
+
+
             var appOfflineFile = FileSystem.Combine(directive.VDirPhysicalPath, "app_offline.htm");
 
             // TODO -- make this nicer
             log.Trace("Writing the application offline file to " + appOfflineFile);
             _fileSystem.WriteStringToFile(appOfflineFile,
                                           "&lt;html&gt;&lt;body&gt;Application is being rebuilt&lt;/body&gt;&lt;/html&gt;");
+        }
+
+        private static void stopPools(Website directive, IPackageLog log)
+        {
+            using (var sm = new ServerManager())
+            {
+                foreach (var pool in sm.ApplicationPools)
+                {
+                    if (!pool.Name.EqualsIgnoreCase(directive.AppPool)) continue;
+
+                    if (pool.State == ObjectState.Started)
+                    {
+                        log.Trace("Stopping pool '{0}'", pool.Name);
+                        pool.Stop();
+                    }
+                }
+
+                sm.CommitChanges();
+            }
         }
 
         public string GetDescription(Website directive)
