a61ff99d91180b062caaf7f6163ec9e1ae5a45a6
jeremydmiller <jeremydmiller@yahoo.com>
2012-11-21 07:30:39 -0600

    added the new OpenManifestCommand

---
+++ src/Bottles/Commands/OpenManifestCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using FubuCore.CommandLine;
+using FubuCore;
+
+namespace Bottles.Commands
+{
+    public class OpenManifestInput
+    {
+        [Description("The physical path or alias for the Bottle directory")]
+        public string BottleDirectory { get; set; }
+    }
+
+    [CommandDescription("Opens the package manifest file in the supplied directory in your text editor", Name = "open-manifest")]
+    public class OpenManifestCommand : FubuCommand<OpenManifestInput>
+    {
+        public override bool Execute(OpenManifestInput input)
+        {
+            var directory = new AliasService().GetFolderForAlias(input.BottleDirectory);
+
+            var packageFile = directory.AppendPath(PackageManifest.FILE);
+            Console.WriteLine("Looking for " + packageFile);
+
+            var system = new FileSystem();
+
+            if (system.FileExists(packageFile))
+            {
+                Console.WriteLine("Opening file...");
+                system.LaunchEditor(packageFile);
+            }
+            else
+            {
+                Console.WriteLine("Could not find a PackageManifest");
+                Console.WriteLine("To create a new PackageManifest, use 'bottles init " + directory + " [PackageName]'");
+            }
+
+
+
+
+            return true;
+        }
+    }
+}
