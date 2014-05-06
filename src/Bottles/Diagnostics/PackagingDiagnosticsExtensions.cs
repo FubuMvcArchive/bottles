1d7b4c792dd2e46d3aa636e06cd7afad77cc1ecf
jeremydmiller <jeremydmiller@yahoo.com>
2014-05-06 08:14:04 -0500

    parallelizing the activator execution.  Closes GH-106

--- src/Bottles/Diagnostics/PackagingDiagnosticsExtensions.cs
+++ src/Bottles/Diagnostics/PackagingDiagnosticsExtensions.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Bottles.Diagnostics
 {
@@ -13,6 +15,20 @@ namespace Bottles.Diagnostics
                 diagnostics.LogExecution(currentTarget, () => continuation(currentTarget, log));
             });
         }
+
+        public static void LogExecutionOnEachInParallel<TItem>(this IBottlingDiagnostics diagnostics, IEnumerable<TItem> targets, Action<TItem, IPackageLog> continuation)
+        {
+            var tasks = targets.Select(currentTarget => {
+                return Task.Factory.StartNew(() => {
+                    var log = diagnostics.LogFor(currentTarget);
+                    diagnostics.LogExecution(currentTarget, () => continuation(currentTarget, log));
+                });
+            }).ToArray();
+
+            Task.WaitAll(tasks);
+        }
+
+        
 
         public static void LogPackages(this IBottlingDiagnostics diagnostics, IPackageLoader loader, IEnumerable<IPackageInfo> packages)
         {
